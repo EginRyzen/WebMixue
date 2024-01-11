14fa7db00ef697360399533dcd9ac6919cb4b024
EginRyzen <muhammadaliagil@gmail.com>
2024-01-11 08:50:38 +0700

    filter button

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ menuToggle.addEventListener('click', function () {
     // Toggle the 'slide' class on the nav element
     nav.classList.toggle('slide');
 });
+
+$(".filter-button").click(function () {
+    let valuDataFilter = $(this).attr("data-filter");
+    if (valuDataFilter === "all") {
+        $(".filter").show(300);
+    } else {
+        $(".filter")
+            .not("." + valuDataFilter)
+            .hide(300);
+        $(".filter")
+            .filter("." + valuDataFilter)
+            .show(300);
+    }
+    if ($(".filter-button").removeClass("active")) {
+        $(this).removeClass("active");
+        $(this).addClass("active");
+    }
+});
